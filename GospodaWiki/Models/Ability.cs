671a3e13ef283e76b55ecccdf49cd46a45107411
Gamber-github <wojciech.kubik1993@gmail.com>
2024-05-30 19:48:59 +0200

    Changes in one of class property

--- GospodaWiki/Models/Ability.cs
+++ GospodaWiki/Models/Ability.cs
@@ -6,6 +6,6 @@
         public string Name { get; set; }
         public string Description { get; set; }
         public string Type { get; set; }
-        public ICollection<CharacterAbility> CharacterAbilities { get; set; }
+        public CharacterAbility CharacterAbility { get; set; }
     }
 }
